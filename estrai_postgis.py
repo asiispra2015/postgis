055928dbf74f6cd8ab1bf3aca9d066d4195ac37a
asiispra@gmail.com <guidofioravanti@yahoo.it>
2018-07-06 11:20:02 +0200

    Corretto l'uso del calendario per identificare le bande nei rasters multilayers. Aggiunto un parametro parametriRAST per decidere quali informazioni temporali acquisire. Corretto bug nel caso in cui vengano richiesti i dati di un solo giorno

--- estrai_postgis.py
+++ estrai_postgis.py
@@ -13,20 +13,36 @@ import pdb
 from functools import reduce
 from datetime import datetime
 
+#su rasters
+parametriRasters={"temperature": "t2m",
+                  "precipitation": "tp",
+                  "surface pressure": "sp",
+                  "planet boundary layer 00": "pbl00",
+                  "planet boundary layer 12": "pbl12",
+                  "u wind": "u10",
+                  "v wind": "v10",
+                  "ndvi": "ndvi",
+                  "dust": "dust",
+                  "aod cams": "aod550"
+                 }
+
 
 # In[3]:
-def estrai_dati(yymmddI,yymmddF):
-    '''
-    Funzione per estrarre i dati vettoriali (non temporali) e raster (temporali) associati
-    alle centraline di pm10.
-    :param yymmddI: data di inizio per i dati temporali (serie di pm10 e parametri raster) nella forma yyyy-mm-dd
-    :param yymmddF: data di fine per i dati temporali (serie di pm10 e parametri raster) nella forma yyyy-mm-dd
-    :returns: un dataframe con i dati di pm10 della centralina nel periodo compreso tra yymmddI e yymmddF e i dati
-    vettoriali (non temporali) e raster (temporali) associati al centroide più vicino alla centralina.
+def estrai_dati(yymmddI,yymmddF,parametriRAST=parametriRasters):
+    
+    '''Funzione per estrarre i dati vettoriali (non temporali) e raster (temporali- giornalieri) associati alle centraline di pm10.
+    --- Parametri ---
+        yymmddI: data di inizio per i dati temporali (serie di pm10 e parametri raster) nella forma yyyy-mm-dd
+        yymmddF: data di fine per i dati temporali (serie di pm10 e parametri raster) nella forma yyyy-mm-dd
+        parametriRAST: parametri temporali che vogliamo estrarre. I parametri vettoriali vengono estratti tutti.
+    --- Returns ---    
+        un dataframe con i dati di pm10 delle centraline nel periodo compreso tra yymmddI e yymmddF, i dati
+        vettoriali (non temporali) e raster (temporali) associati al centroide della griglia di ciascuna centralina.
     '''
 
-    annoI=yymmddI[0:4]
-    annoF=yymmddF[0:4]
+    #i dati nel database riguardano il 2015. annoI e annoF vanno cambiati solo se cambiano i dati sul database
+    annoI,annoF=("2015","2015")
+    giornoI,giornoF=(annoI+"-01-01",annoI+"-12-31")
 
     try:
         data1=datetime.date(datetime.strptime(yymmddI,"%Y-%m-%d"))
@@ -44,58 +60,35 @@ def estrai_dati(yymmddI,yymmddF):
         sys.exit("yymmddF deve essere posteriore a yymmddI")
     
     
-    #calendario completo
-    calendarioCompleto=pd.Series(pd.date_range(start=data1,end=data2,freq="D"))
+    #calendario completo del 2015 o del periodo che coprono i dati sul database postgis
+    calendarioCompleto=pd.Series(pd.date_range(start=giornoI,end=giornoF,freq="D"))
 
-    #calendario relativo al periodo di interesse
+    #calendario relativo al periodo di interesse passato alla funzione
     calendario=pd.Series(pd.date_range(start=yymmddI,end=yymmddF,freq="D"))
 
-
+    #le date in "calendario" rientrano tutte nel periodo coperto dai dati sul database postgis?? Date oltre
+    #la disponibilità sul database postgis vanno eliminate
+    calendarioEffettivo=calendario.loc[(calendario >= giornoI) & (calendario<= giornoF)]
     # In[4]:
 
 
-    #lista_bande mi dice la posizione dei layer nei vari rasters
-    lista_bande=np.where(np.in1d(calendarioCompleto.tolist(),calendario.tolist()))
+    #lista_bande: devo trovare i layer (nei raster temporali) che corrispondono al periodo dato da "calendarioEffettivo"
+    #In pratica: sto dicendo..quale layer corrisponde (ad esempio) al 2015-03-04?
+    lista_bande=np.where(np.in1d(calendarioCompleto.tolist(),calendarioEffettivo.tolist()))
     lista_bande=lista_bande[0].tolist()
 
-
-    # In[5]:
-
-
-    anni=np.unique(calendario.dt.year)
-
-
-    # In[6]:
+    if len(lista_bande)==0:
+        sys.exit("Nessuna delle date passate è disponibile nel database postgis, esco!")
+    elif len(lista_bande) < len(calendario):
+        print("Alcune delle date richieste non sono disponibili sul database postgis!")
 
 
-    #parametri di interesse
-    parametri=("t2m","tp","sp","pbl00","pbl12","u10","v10","ndvi","dust","aod550")
+    #gli anni in calendarioEffettivo     
+    anni=np.unique(calendarioEffettivo.dt.year)
 
 
     # In[7]:
-
-
     motore=create_engine("postgres://guido:postgres2015@localhost/asiispra")
-
-
-    # In[8]:
-
-
-    #questa funzione utilizzava una query
-    #def estraiDati(lista,parametro,band):
-    #    '''Funzione per estrarre i dati raster dal database postgis in corrispondenza delle stazioni'''
-    #    if band <0 or band > 366:
-    #        sys.exit("Numero di banda errato")
-
-        #query: solo le centraline che ci interessano, sottoinsieme di tutte quelle dispoinili
-    #    myquery="SELECT id_centralina,idcell,St_value({0}.rast,{1},geom) AS {0},geom FROM rgriglia.{0},centraline.pm10_con_dati WHERE St_intersects(geom,{0}.rast)".format(parametro,band)
-    #    dati=pd.read_sql_query(myquery,con=motore)
-    #    dati['banda']=banda
-    #    lista.append(dati)
-    #    return None
-
-
-    # In[44]:
 
 
     #questa funzione richiama la funzione rgriglia.get_raster_value in postgresql
@@ -120,31 +113,23 @@ def estrai_dati(yymmddI,yymmddF):
 
     listaFinale=[]
 
-    for param in parametri:
+    for nome_esteso,param in parametriRAST.items():
         listaOut=[]
-        
-        for banda in lista_bande:
 
+        for banda in lista_bande:
             estraiRasterDati(lista=listaOut,parametro=param,band=(banda+1))
+            
         out=pd.concat(listaOut,axis=0,ignore_index=True)
         listaFinale.append(out)
-
-
-    # In[46]:
 
 
     #reduce dei vari data frame
     cenPM10=reduce(lambda x,y: pd.merge(x,y,how="left",on=["id_centralina","idcell","geom","banda"]),listaFinale)
 
 
-    # In[48]:
-
-
     #creazione della colonna geometry
     geom=cenPM10.apply(lambda x: shapely.wkb.loads(x['geom'],hex=True),axis=1)
 
-
-    # In[49]:
 
 
     cenPM10.geom=geom
@@ -162,13 +147,10 @@ def estrai_dati(yymmddI,yymmddF):
 
     #centraline che hanno dati
     id_centraline=str(tuple(np.unique(cenPM10.id_centralina).tolist()))
-    #crea tupla con anno mese e giorno per query
-    yymmdd=tuple(calendario.dt.date.astype(str).tolist())
-
-
-    # In[52]:
-
-
+    
+    #crea la sequenza di date da passare a postgresql, seuqenza racchiusa tra parentesi e con le date tra apici
+    stringa_date_per_query="("+ ",".join("'{0}'".format(w) for w in calendarioEffettivo.dt.date.astype(str).tolist() ) + ")"
+    
     #acquisizione dati del PM10
     #for id in id_centraline:
     myquery='''SELECT id_centralina,
@@ -177,13 +159,10 @@ def estrai_dati(yymmddI,yymmddF):
                 FROM 
                       serie.pm10 
                 WHERE 
-                      (id_centralina IN {0}) AND (data_record IN {1})'''.format(id_centraline,yymmdd)
+                      (id_centralina IN {0}) AND (data_record IN {1})'''.format(id_centraline,stringa_date_per_query)
+    
     #valori giornalieri del pm10
     pm10=pd.read_sql_query(con=motore,sql=myquery)
-
-
-
-    # In[53]:
 
 
     pm10.data_record=pd.to_datetime(pm10.data_record,format="%Y-%m-%d")
@@ -221,21 +200,12 @@ def estrai_dati(yymmddI,yymmddF):
     serie=pd.merge(pm10,cenPM10,how="left",on=["id_centralina","banda"])
 
 
-    # In[57]:
-
-
-    #acquisizione dei dati dai centroidi di griglia
+    #acquisizione dei dati dai centroidi di griglia: sfrutta la funzione in plsql vgriglia.get_vector_value() memorizzata su postgis
     parametriPuntuali=pd.read_sql_query(con=motore,sql="SELECT * FROM vgriglia.get_vector_value()")
-
-
-    # In[58]:
 
 
     #uniamo i dati vettoriali con i dati raster
     dati=pd.merge(serie,parametriPuntuali,how="inner",left_on="idcell",right_on="id")
-
-
-    # In[59]:
 
 
     return dati
